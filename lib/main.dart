01cf5c0e960a1e633f1b85604625b65386645054
nexlay <primenickprime@gmail.com>
2020-03-15 10:41:54 +0100

    Added the second page 'About'. Navigator. Image asset.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'dart:math';
+
+import 'AboutPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -74,6 +77,7 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -87,143 +91,153 @@ class _HomePageState extends State<HomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.info_outline), onPressed: _aboutApp),
+        ],
       ),
       body: SingleChildScrollView(
         child: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 95.0),
-            ),
-            Text(
-              'Result',
-              style: TextStyle(fontSize: 20.0),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-            ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: _fontSize, color: Colors.red),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-            ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 5.0),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: TextFormField(
-                    controller: _minRetrieveController,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter some number';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Min number',
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 95.0),
+              ),
+              Text(
+                'Result',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+              ),
+              Text(
+                '$_counter',
+                style: TextStyle(fontSize: _fontSize, color: Colors.red),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+              ),
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5.0),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: TextFormField(
+                      controller: _minRetrieveController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some number';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Min number',
+                      ),
+                      keyboardType: TextInputType.numberWithOptions(),
                     ),
-                    keyboardType: TextInputType.numberWithOptions(),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 5.0),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: TextFormField(
-                    controller: _maxRetrieveController,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter some number';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Max number',
+                  Padding(
+                    padding: const EdgeInsets.only(right: 5.0),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: TextFormField(
+                      controller: _maxRetrieveController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some number';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Max number',
+                      ),
+                      keyboardType: TextInputType.numberWithOptions(),
                     ),
-                    keyboardType: TextInputType.numberWithOptions(),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 5.0),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 18.0),
-            ),
-            ButtonBar(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ButtonTheme(
-                  minWidth: 150.0,
-                  height: 55.0,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.red)),
-                  child: RaisedButton(
-                    onPressed: () {
-                      _randomCounter();
-                    },
-                    child: Text('Generate', style: TextStyle(fontSize: 16.0)),
-                    textColor: Colors.white,
-                    color: Colors.red,
-                    splashColor: Colors.white,
+                  Padding(
+                    padding: const EdgeInsets.only(right: 5.0),
                   ),
-                ),
-                ButtonTheme(
-                  minWidth: 100.0,
-                  height: 55.0,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.white)),
-                  child: RaisedButton(
-                    onPressed: () {
-                      _minRetrieveController.clear();
-                      _maxRetrieveController.clear();
-                      setState(() {
-                        _counter = 0;
-                      });
-                    },
-                    child: Text(
-                      'Clear',
-                      style: TextStyle(fontSize: 16.0),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 18.0),
+              ),
+              ButtonBar(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: 150.0,
+                    height: 55.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.red)),
+                    child: RaisedButton(
+                      onPressed: () {
+                        _randomCounter();
+                      },
+                      child: Text('Generate', style: TextStyle(fontSize: 16.0)),
+                      textColor: Colors.white,
+                      color: Colors.red,
+                      splashColor: Colors.white,
                     ),
-                    textColor: Colors.red,
-                    color: Colors.white,
-                    splashColor: Colors.red,
                   ),
-                ),
-              ],
-            ),
-          ],
+                  ButtonTheme(
+                    minWidth: 100.0,
+                    height: 55.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.white)),
+                    child: RaisedButton(
+                      onPressed: () {
+                        _minRetrieveController.clear();
+                        _maxRetrieveController.clear();
+                        setState(() {
+                          _counter = 0;
+                        });
+                      },
+                      child: Text(
+                        'Clear',
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      textColor: Colors.red,
+                      color: Colors.white,
+                      splashColor: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
+
+  void _aboutApp() {
+    var _route = MaterialPageRoute(
+        builder: (BuildContext context) => AboutPage());
+    Navigator.of(context).push(_route);
+  }
 }
+
